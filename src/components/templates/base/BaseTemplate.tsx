0cb034b02686112470a263fba67bb7ffcd68c1c6
Gedroke <ibrahim.elmhadri@gmail.com>
2022-09-18 14:46:32 +0200

    fix: fixing path of tailwindcss config purge, enable intelisense for js/ts, clean the template

---
+++ src/components/templates/base/BaseTemplate.tsx
@@ -0,0 +1,7 @@
+export interface IBaseTemplate {}
+
+const BaseTemplate: React.FC<IBaseTemplate> = () => {
+  return <div>Hello World!</div>;
+};
+
+export default BaseTemplate;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,21 @@
-const defaultTheme = require("tailwindcss/defaultTheme")
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       colors: {
-        primary: "#00040f",
-        secondary: "#00f6ff",
-        dimWhite: "rgba(255, 255, 255, 0.7)",
-        dimBlue: "rgba(9, 151, 124, 0.1)",
+        primary: '#00040f',
+        secondary: '#00f6ff',
+        dimWhite: 'rgba(255, 255, 255, 0.7)',
+        dimBlue: 'rgba(9, 151, 124, 0.1)',
       },
       fontFamily: {
-        poppins: ["Poppins", "sans-serif"],
-        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
       },
-    },
-    screens: {
-      xs: "480px",
-      ss: "620px",
-      sm: "768px",
-      md: "1060px",
-      lg: "1200px",
-      xl: "1700px",
     },
   },
   plugins: [
@@ -32,5 +23,5 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/line-clamp'),
     require('@tailwindcss/aspect-ratio'),
-  ]
-}
+  ],
+};
